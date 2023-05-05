202b0af5cf2ab66621d9295f6c263f32c46b4899
Cosmin Lucaci <cosmin.lucaci@emea.nttdata.com>
2023-05-05 20:08:02 +0300

    find elections

--- src/main/java/licenta/controller/ElectionsController.java
+++ src/main/java/licenta/controller/ElectionsController.java
@@ -5,6 +5,7 @@ import licenta.service.ElectionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,4 +19,10 @@ public class ElectionsController {
     public ResponseEntity<List<Elections>> getElections() {
         return ResponseEntity.ok(service.findAllElections());
     }
+
+    @GetMapping("/election")
+    public ResponseEntity<Elections> getElectionByName(@RequestParam String electionName) {
+        return ResponseEntity.ok(service.findElectionByName(electionName));
+    }
+
 }
